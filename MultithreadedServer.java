acc5f73210215e8d0fe0a0ca5f110a5b768edcd2
Erica Braunschweig <erica.braunschweig@outlook.com>
2016-05-10 03:48:37 +0000

    one giant update because we suck at version control

--- MultithreadedServer.java
+++ MultithreadedServer.java
@@ -4,17 +4,101 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-// TO DO: Task is currently an ordinary class.
-// You will need to modify it to make it a task,
-// so it can be given to an Executor thread pool.
-//
-class Task {
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+//https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/ThreadPoolExecutor.html
+
+
+class CachedAccount {
+    
+    Account account;
+    String name;
+    int initial_value;
+    int current_value;
+    public boolean writing_account;
+    public boolean reading_account;
+    
+    public CachedAccount(Account acc, String word){
+        account = acc;
+        initial_value = account.peek();
+        current_value = initial_value;
+        name = word;
+        writing_account = false;
+        reading_account = false;
+    }
+    
+    public Account GetCachedAccount(){
+        return left_account;
+    }
+    
+    public void SetReading(){
+        reading_account = true;
+    }
+    
+    public void SetWriting(){
+        writing_account = true;
+    }
+    
+    public void SetValue(int value){
+        current_value = value;
+    }
+    
+    public String GetName() {
+        return name;
+    }
+    
+    public void open(){
+        bool opened = false;
+        try{
+            if (writing_account){
+                account.open(true);
+            }
+            opened = true;
+            if (reading_account){
+                account.open(false);
+            }
+        }
+        catch(TransactionAbortException exception){
+            if (opened){
+                close();
+            }
+        }
+    }
+    
+    public void close(){
+        account.close();
+    }
+    
+    public int peek(){
+        return current_value;
+    }
+    
+    public void update(int newValue) {
+        try {
+            account.update(int newValue);
+        }
+        catch(TransactionUsageError exc){
+            throw exc;
+        }
+    }
+}
+
+class Task implements Runnable {
+    
     private static final int A = constants.A;
     private static final int Z = constants.Z;
     private static final int numLetters = constants.numLetters;
 
     private Account[] accounts;
     private String transaction;
+    private List<CachedAccount> cached_accounts = new ArrayList<CachedAccount>();
+    
 
     // TO DO: The sequential version of Task peeks at accounts
     // whenever it needs to get a value, and opens, updates, and closes
@@ -34,7 +118,7 @@ class Task {
     // You probably want to change it to return a reference to an
     // account *cache* instead.
     //
-    private Account parseAccount(String name) {
+    private CachedAccount parseAccount(String name, boolean forWriting) {
         int accountNum = (int) (name.charAt(0)) - (int) 'A';
         if (accountNum < A || accountNum > Z)
             throw new InvalidTransactionError();
@@ -42,10 +126,37 @@ class Task {
         for (int i = 1; i < name.length(); i++) {
             if (name.charAt(i) != '*')
                 throw new InvalidTransactionError();
-            accountNum = (accounts[accountNum].peek() % numLetters);
-            a = accounts[accountNum];
+            int searcher = AlreadyCached(accountNum + 'A');
+            if (searcher == -1){
+                CachedAccount ca = new CachedAccount(a, (char) (accountNum + 'A'));
+                ca.SetReading();
+                cached_accounts.add(ca);
+                accountNum = (accounts[accountNum].peek() % numLetters);
+                a = accounts[accountNum];
+            }
+            else{
+                accountNum = cached_accounts[searcher].peek();
+                a = accounts[accountNum];
+            }
         }
-        return a;
+        int searcher = AlreadyCached(accountNum + 'A');
+        CachedAccount ca;
+        if (searcher == -1){
+            ca = new CachedAccount(a, (char) (accountNum + 'A'));
+        }
+        else{
+            ca = cached_accounts[searcher];
+        }
+        if (forWriting){
+            ca.SetWriting();
+        }
+        else{
+            ca.SetReading();
+        }
+        if (searcher == -1){
+            cached_accounts.add(ca);
+        }
+        return ca;
     }
 
     private int parseAccountOrNum(String name) {
@@ -53,40 +164,132 @@ class Task {
         if (name.charAt(0) >= '0' && name.charAt(0) <= '9') {
             rtn = new Integer(name).intValue();
         } else {
-            rtn = parseAccount(name).peek();
+            rtn = parseAccount(name, false).peek();
         }
         return rtn;
+    }
+    
+    public void SortAccounts(){
+        
+        for (int x = 1; x < cached_accounts.length; x++){
+            int y = x - 1;
+            int sort_result = cached_accounts[y].GetName().compareTo(cached_accounts[x].GetName());
+            
+            //previous letter comes after next letter
+            if (sort_result > 0) {
+                
+                //swap the account
+                CachedAccount temp_account = cached_accounts[y];
+                cached_accounts[y] = cached_accounts[x];
+                cached_accounts[x] = temp_account;
+                x = 0;
+                
+            }
+        }
+    }
+    
+    public int AlreadyCached(String name) {
+        for (int i = 0; i < cached_accounts.length; ++i) {
+            if (cached_accounts[i].GetName() == name) {
+                return i;
+            }
+        }
+        
+        return -1;
     }
 
     public void run() {
         // tokenize transaction
+        
         String[] commands = transaction.split(";");
+        
+        while(true){
 
-        for (int i = 0; i < commands.length; i++) {
-            String[] words = commands[i].trim().split("\\s");
-            if (words.length < 3)
-                throw new InvalidTransactionError();
-            Account lhs = parseAccount(words[0]);
-            if (!words[1].equals("="))
-                throw new InvalidTransactionError();
-            int rhs = parseAccountOrNum(words[2]);
-            for (int j = 3; j < words.length; j+=2) {
-                if (words[j].equals("+"))
-                    rhs += parseAccountOrNum(words[j+1]);
-                else if (words[j].equals("-"))
-                    rhs -= parseAccountOrNum(words[j+1]);
-                else
+            for (int i = 0; i < commands.length; i++) {
+                String[] words = commands[i].trim().split("\\s");
+                if (words.length < 3)
                     throw new InvalidTransactionError();
+                    
+                CachedAccount lhs = parseAccount(words[0], true);
+                
+                if (!words[1].equals("="))
+                    throw new InvalidTransactionError();
+                    
+                int rhs = parseAccountOrNum(words[2]);
+
+                
+                for (int j = 3; j < words.length; j+=2) {
+                    if (words[j].equals("+")){
+                        int rhs2 = parseAccountOrNum(words[j+1]);
+                        rhs += rhs2;
+                    }
+                    else if (words[j].equals("-")){
+                        int rhs2 = parseAccountOrNum(words[j+1]);
+                        rhs -= rhs2;
+                    }
+                    else
+                        throw new InvalidTransactionError();
+                }
+                lhs.SetValue(rhs);
             }
-            try {
-                lhs.open(true);
-            } catch (TransactionAbortException e) {
-                // won't happen in sequential version
+            int opened_accounts = 0;
+            try{
+                SortAccounts();
+                for (int x = 0; x < cached_accounts.length; x++){
+                    cached_accounts[x].open();
+                    opened_accounts += 1;
+                }
+                catch (TransactionAbortException e) {
+                        // won't happen in sequential version
+                        while (opened_accounts > 0){
+                            cached_accounts[opened_accounts - 1].close();
+                            opened_accounts -= 1;
+                        }
+                        try {
+                            Thread.sleep(100);  // ms
+                        } catch(InterruptedException e) {}
+                        continue;
+                    }
+                }
             }
-            lhs.update(rhs);
-            lhs.close();
+            
+            try{
+                for (int x = 0; x < cached_accounts.length; x++){
+                    cached_accounts[x].verify(cached_accounts[x].initial_value);
+                }
+            }
+            catch(TransactionAbortException e){
+                for (int x = 0; x < cached_accounts.length; x++){
+                    cached_accounts[x].close();   
+                }
+                try {
+                    Thread.sleep(100);  // ms
+                } catch(InterruptedException e) {}
+                continue;
+            }
+            
+            try{
+                for (int x = 0; x < cached_accounts.length; x++){
+                    if (cached_accounts[x].writing_account){
+                        cached_accounts[x].update(cached_accounts[x].peek());
+                    }
+                }
+            }
+            catch(TransactionUsageError e){
+                break;
+            }
+            
+            try{
+                for (int x = 0; x < cached_accounts.length; x++){
+                    cached_accounts.close();
+                }
+            }
+            catch(TransactionUsageError e){
+                break;
+            }
+            
+            System.out.println("commit: " + transaction);
         }
-        System.out.println("commit: " + transaction);
     }
 }
 
@@ -105,14 +308,26 @@ public class MultithreadedServer {
 
         // TO DO: you will need to create an Executor and then modify the
         // following loop to feed tasks to the executor instead of running them
-        // directly.  
+        // directly.  DONE?
+        
+        private static Executor e = Executors.newCachedThreadPool();
 
         while ((line = input.readLine()) != null) {
-            Task t = new Task(accounts, line);
-            t.run();
+            e.execute(new Task(accounts, line));
+            
+            //Task t = new Task(accounts, line);
+            //t.run();
+        }
+        
+        if (!e.awaitTermination(60, TimeUnit.SECONDS)){
+            e.shutdownNow();
+            if (!e.awaitTermination(60, TimeUnit.SECONDS)){
+                System.err.println("Executor did not terminate");
+            }
+        } catch (InterruptedException ie){
+            e.shutdownNow();
         }
         
         input.close();
-
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # prog_lang_hmwk_9
 Programming Languages Homework 9 - Java Concurrency
+Created by The Sexy God (hayned2) and Fuhrer Brown Swag (braune)
